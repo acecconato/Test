11f974b025beef3a76652b836ff0e8ac2cdc6f68
Anthony Cecconato <antho.cecc@gmail.com>
2022-11-18 14:50:31 +0100

    Add Json web token authentication

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -5,15 +5,17 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ClientRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: ClientRepository::class)]
 #[ApiResource]
 class Client
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\Column(type: 'uuid', unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private ?Uuid $id = null;
 
     #[ORM\Column(length: 20)]
     private ?string $firstName = null;
@@ -27,7 +29,7 @@ class Client
     #[ORM\Column(length: 20, nullable: true)]
     private ?string $phoneNumber = null;
 
-    public function getId(): ?int
+    public function getId(): ?Uuid
     {
         return $this->id;
     }
---
+++ tests/AuthenticationTest.php
@@ -0,0 +1,73 @@
+<?php
+// tests/AuthenticationTest.php
+
+namespace App\Tests;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\User;
+use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
+
+class AuthenticationTest extends ApiTestCase
+{
+    use ReloadDatabaseTrait;
+
+    /**
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Exception
+     */
+    public function testLogin(): void
+    {
+        $client    = self::createClient();
+        $container = self::getContainer();
+
+        $user = new User();
+        $user->setEmail('test@example.com');
+        $user->setPassword(
+            $container->get('security.user_password_hasher')->hashPassword($user, '$3CR3T')
+        );
+
+        $manager = $container->get('doctrine')->getManager();
+        $manager->persist($user);
+        $manager->flush();
+
+        // not authenticated
+        $client->request('GET', '/api/clients');
+        $this->assertResponseStatusCodeSame(401);
+
+        $response = $client->request('POST', '/authentication_token', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json'    => [
+                'email'    => 'test@example.com',
+                'password' => 'wrong',
+            ],
+        ]);
+
+        // invalid credentials
+        $this->assertResponseStatusCodeSame(401);
+
+        $response = $client->request('POST', '/authentication_token', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json'    => [
+                'email'    => 'test@example.com',
+                'password' => '$3CR3T',
+            ],
+        ]);
+
+        $json = $response->toArray();
+
+        $this->assertResponseIsSuccessful();
+        $this->assertArrayHasKey('token', $json);
+
+        // test not authorized
+        $client->request('GET', '/api/clients');
+        $this->assertResponseStatusCodeSame(401);
+
+        // test authorized
+        $client->request('GET', '/api/clients', ['auth_bearer' => $json['token']]);
+        $this->assertResponseIsSuccessful();
+    }
+}
